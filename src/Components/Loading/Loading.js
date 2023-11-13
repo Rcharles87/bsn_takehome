5e46ccf5b363bdfb659c9e280c9dee17a31a3866
rae charles <r_charles87@yahoo.com>
2023-11-13 10:07:03 -0500

    added loadind css file and styling, imported loading icon and default logic for loading

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Loading.css";
+import { CircularProgress } from "@mui/material";
 
 function Loading() {
   return (
-    <div>Loading</div>
-  )
+    <div className="Loading">
+      <div>
+        <CircularProgress />
+      </div>
+
+      <h4>Loading...</h4>
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
