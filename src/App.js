d88a238ac6feb647dd118fab2f129f8e974a7e98
rae charles <r_charles87@yahoo.com>
2023-11-13 22:00:01 -0500

    started quiz rendering, added render content function on app js to handle load error and game state, added quiz Component folder and render content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import "./App.css";
-import Quiz from "./Components/Quiz/Quiz";
+import Quiz from "./Components/AllQuizes/AllQuizes";
 import Loading from "./Components/Loading/Loading";
 import Error from "./Components/Error/Error";
 import Container from "./Components/Container/Container";
 
 function App() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
+
+
+  const renderContent = () => {
+    if(loading){
+      return <Loading/>
+    }else if(error){
+      return <Error error={error}/>
+    }else{
+      return <Quiz setLoading={setLoading}/>
+    }
+  }
 
   return <div className="App">
     <Container center={Boolean(error || loading)} scroll={false}>
-    hey world, hey!!!
+      {renderContent()}
     </Container>
     </div>;
 }
